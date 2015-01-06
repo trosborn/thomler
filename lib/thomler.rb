c4698219d280aec7d2156d3bedc6ed1ef5bee02d
Thomas Osborn <trosborn@gmail.com>
2015-01-06 15:04:52 +1200

    updates to thomler 0.2.1, adding exception handling

--- lib/thomler.rb
+++ lib/thomler.rb
@@ -6,14 +6,14 @@ module Thomler
   class Application
     def call env
       if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content-Type' => 'text/html'}, ["text"]]
+        return [404, {'Content-Type' => 'text/html'}, ["404, File not found"]]
       end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       begin
         text = controller.send(act)
       rescue Exception
-        return [404, {'Content-Type' => 'text/html'}, ["text"]]
+        return [500, {'Content-Type' => 'text/html'}, ["500", "Method not found"]]
       end
       [200, {'Content-Type' => 'text/html'}, ["text"]]
     end
