b3de8e566d89579e7e09d5cd71bd5afd15852157
Thomas Osborn <trosborn@gmail.com>
2015-01-24 16:19:01 -0800

    finishes chapter 4

--- lib/thomler.rb
+++ lib/thomler.rb
@@ -9,12 +9,12 @@ module Thomler
   class Application
     def call env
       if env['PATH_INFO'] == '/favicon.ico'
-        [404, {'Content-Type' => 'text/html'}, ["404", "File not found"]]
+        [404, {'Content-Type' => 'text/html'}, []]
       end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, ["#{text}"]]
+      [200, {'Content-Type' => 'text/html'}, [text]]
     end
   end
 
--- lib/thomler/controller.rb
+++ lib/thomler/controller.rb
@@ -9,10 +9,16 @@ module Thomler
     end
 
     def render view_name, locals = {}
-      filename = File.join "app", "views", "#{view_name}.html.erb"
+      filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(:env => env)
+    end
+
+    def controller_name
+      klass = self.class
+      klass = klass.to_s.gsub /Controller$/, ""
+      Thomler.to_underscore klass
     end
   end
 end
