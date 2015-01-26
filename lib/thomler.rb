4f1c7bf60695771ef88b78baffb4eff0ec076763
Thomas Osborn <trosborn@gmail.com>
2015-01-25 22:03:09 -0800

    finishes chapter 6

--- lib/thomler.rb
+++ lib/thomler.rb
@@ -12,10 +12,16 @@ module Thomler
       if env['PATH_INFO'] == '/favicon.ico'
         [404, {'Content-Type' => 'text/html'}, []]
       end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      end
     end
   end
 
--- lib/thomler/controller.rb
+++ lib/thomler/controller.rb
@@ -1,8 +1,16 @@
 require "thomler/file_model"
+require "rack/request"
 
 module Thomler
   class Controller
     include Thomler::Model
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
 
     def initialize env
       @env = env
@@ -23,6 +31,20 @@ module Thomler
       klass = self.class
       klass = klass.to_s.gsub /Controller$/, ""
       Thomler.to_underscore klass
+    end
+
+    def response text, status = 200, headers = {}
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response
+      @response
+    end
+
+    def render_response *args
+      response(render(*args))
     end
   end
 end
